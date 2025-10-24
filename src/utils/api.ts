c0e64f17f1089798468e15ccab7fc1d27da29b2b
Hassan Adam <hassanad@Hassan-Mac-mini.local>
2025-10-24 13:32:40 +0200

    feature: add request handler

---
+++ src/utils/api.ts
@@ -0,0 +1,57 @@
+import type { AxiosResponse } from "axios";
+import axios from "axios";
+
+export type ApiResponse<T> = {
+	status: boolean;
+	data: T;
+};
+
+type BaseRequest<T, V> = (params?: T) => Promise<AxiosResponse<V>>;
+type BaseResponse<V> = Promise<
+	{ status: true; data: SuccessResponseData<V> } | ErrorResponse
+>;
+
+export type ErrorResponse = {
+	status: false;
+	error?: AppError;
+};
+
+export type AppError = {
+	id: string | null;
+	message: string | null;
+	detailedMessage: string | null;
+	errorObject: Record<string, unknown> | null;
+};
+
+type SuccessResponseData<V> = {
+	StatusCode: string | null;
+	Result: V;
+};
+
+export const requestHandler =
+	<T, V>(request: BaseRequest<T, V>) =>
+	async (params?: T): BaseResponse<V> => {
+		try {
+			const response = await request(params);
+
+			const data = response.data as SuccessResponseData<V>;
+
+			return { status: true, data };
+		} catch (e: unknown) {
+			if (axios.isAxiosError<AppError>(e)) {
+				return {
+					status: false,
+					error: e.response?.data,
+				};
+			}
+			return {
+				status: false,
+				error: {
+					id: null,
+					message: e instanceof Error ? e.message : "Unknown error occurred",
+					detailedMessage: null,
+					errorObject: null,
+				},
+			};
+		}
+	};
